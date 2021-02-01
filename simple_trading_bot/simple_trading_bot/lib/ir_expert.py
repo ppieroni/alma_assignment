a38da09aa6b92136caefd323b4b7f705d0e7c088
Pablo P <pepieroni@gmail.com>
2021-02-01 02:15:02 -0300

    trade arb by tenure + tests

--- simple_trading_bot/simple_trading_bot/lib/ir_expert.py
+++ simple_trading_bot/simple_trading_bot/lib/ir_expert.py
@@ -1,44 +1,50 @@
+from collections import defaultdict
 
 
 class IRExpert:
     DAYS_IN_A_YEAR = 365
 
     def __init__(self, instrument_expert, rofex_proxy, yfinance_md_feed):
-        self._futures_by_underlier_ticker = instrument_expert.rofex_instrument_by_underlier()
+        self._futures_by_underlier_ticker = instrument_expert.tradeable_rofex_instruments_by_underlier_ticker()
+        self._maturiries_by_ticker = instrument_expert.maturities_of_tradeable_tickers()
         self._rofex_proxy = rofex_proxy
         self._yfinance_md_feed = yfinance_md_feed
-        self._taker_rates = {}
-        self._offered_rates = {}
+        self._taker_rates = defaultdict(dict)
+        self._offered_rates = defaultdict(dict)
 
     def update_rates(self):
         underlier_prices = self._yfinance_md_feed.last_prices()
         future_bids = self._rofex_proxy.bids()
         future_asks = self._rofex_proxy.asks()
-        self._taker_rates = {}
-        self._offered_rates = {}
+        self._taker_rates = defaultdict(dict)
+        self._offered_rates = defaultdict(dict)
         for ticker, underlier_price in underlier_prices.items():
             for future in self._futures_by_underlier_ticker[ticker]:
                 future_ticker = future.ticker()
                 days_to_maturity = future.days_to_maturity()
+                maturity_tag = self._maturiries_by_ticker[future_ticker]
                 if future_ticker in future_bids:
-                    self._taker_rates[future_ticker] = self._implicit_rate(
+                    self._taker_rates[maturity_tag][future_ticker] = self._implicit_rate(
                         future_bids[future_ticker].price,
                         underlier_price,
                         days_to_maturity)
                 if future_ticker in future_asks:
-                    self._offered_rates[future_ticker] = self._implicit_rate(
+                    self._offered_rates[maturity_tag][future_ticker] = self._implicit_rate(
                         future_asks[future_ticker].price,
                         underlier_price,
                         days_to_maturity)
 
-    def max_taker_rate(self):
-        return max(self._taker_rates.items(), key=lambda each: each[1])
+    def max_taker_rate(self, maturity_tag):
+        return max(self._taker_rates[maturity_tag].items(), key=lambda each: each[1])
 
-    def min_offered_rate(self):
-        return min(self._offered_rates.items(), key=lambda each: each[1])
+    def min_offered_rate(self, maturity_tag):
+        return min(self._offered_rates[maturity_tag].items(), key=lambda each: each[1])
 
     def ready(self):
         return self._taker_rates and self._offered_rates
+
+    def maturiry_ready_to_trade(self, maturity_tag):
+        return self._taker_rates[maturity_tag] and self._offered_rates[maturity_tag]
 
     def _implicit_rate(self, maturity_price, current_price, days_to_maturity):
         """Anualized implicit rate computed using Actual/DAYS_IN_A_YEAR day count convention and daily compounding"""
--- simple_trading_bot/simple_trading_bot/lib/trader.py
+++ simple_trading_bot/simple_trading_bot/lib/trader.py
@@ -14,44 +14,78 @@ class Trader:
             yfinance_md_feed,
             data_update_watchman):
         self._futures_by_ticker = instrument_expert.rofex_instruments_by_ticker()
+        self._maturity_tags = instrument_expert.tradeable_maturity_tags()
         self._ir_expert = ir_expert
         self._rofex_proxy = rofex_proxy
         self._yfinance_md_feed = yfinance_md_feed
         self._data_update_watchman = data_update_watchman
 
+    def evaluate_and_trade_each_maturiry(self):
+        for maturity_tag in self._maturity_tags:
+            if self._ir_expert.maturiry_ready_to_trade(maturity_tag):
+                self.evaluate_and_trade_single_maturity(maturity_tag)
 
-    def evaluate_and_trade(self):
-        ticker_to_sell, max_taker_rate = self._ir_expert.max_taker_rate()
-        ticker_to_buy, min_offered_rate = self._ir_expert.min_offered_rate()
+    def evaluate_and_trade_single_maturity(self, maturity_tag):
+        ticker_to_sell, max_taker_rate = self._ir_expert.max_taker_rate(maturity_tag=maturity_tag)
+        ticker_to_buy, min_offered_rate = self._ir_expert.min_offered_rate(maturity_tag=maturity_tag)
+        #Strong assumption: transaction cost can be expressed as a constant rate difference
         if not max_taker_rate - min_offered_rate > tc.TRANSACITON_COST:
             return
         future_to_buy = self._futures_by_ticker[ticker_to_buy]
         future_to_sell = self._futures_by_ticker[ticker_to_sell]
-        underlier_to_sell = future_to_buy.underlier_ticker()
+
         underlier_to_buy = future_to_sell.underlier_ticker()
+        underlier_buy_price = self._yfinance_md_feed.price(underlier_to_buy)
+        underlier_to_sell = future_to_buy.underlier_ticker()
+        underlier_sell_price = self._yfinance_md_feed.price(underlier_to_sell)
 
         future_asks = self._rofex_proxy.asks()
         future_bids = self._rofex_proxy.bids()
         available_buy_size = future_asks[ticker_to_buy].size
         available_sell_size = future_bids[ticker_to_sell].size
         amount_to_trade = min(
-            available_buy_size * future_to_buy.contract_size(),
-            available_sell_size * future_to_sell.contract_size())
-        buy_size = int(amount_to_trade / future_to_buy.contract_size())
-        sell_size = int(amount_to_trade / future_to_sell.contract_size())
+            available_buy_size * future_to_buy.contract_size() * underlier_buy_price,
+            available_sell_size * future_to_sell.contract_size() * underlier_sell_price)
+        buy_size = int(amount_to_trade / future_to_buy.contract_size() / underlier_buy_price)
+        sell_size = int(amount_to_trade / future_to_sell.contract_size() / underlier_sell_price)
         buy_price = future_asks[ticker_to_buy].price
         sell_price = future_bids[ticker_to_sell].price
+        underlier_buy_size = sell_size * future_to_sell.contract_size()
+        underlier_sell_size = buy_size * future_to_buy.contract_size()
+        trade_rate_profit = (max_taker_rate - min_offered_rate)
+        av_position_to_take = (underlier_buy_size * underlier_buy_price +
+                               underlier_sell_size * underlier_sell_price) * 0.5
 
-        if not self._data_update_watchman.should_update():
+        if not self._data_update_watchman.should_update() and (buy_size * sell_size) > 0:
             buy_order = self._rofex_proxy.place_order(
                 ticker=ticker_to_buy,
                 side=pyRofex.Side.BUY,
-                size=amount_to_trade,
+                size=buy_size,
                 price=buy_price,
+                time_in_force=pyRofex.TimeInForce.FillOrKill,
                 order_type=pyRofex.OrderType.LIMIT)
             sell_order = self._rofex_proxy.place_order(
                 ticker=ticker_to_sell,
                 side=pyRofex.Side.SELL,
-                size=amount_to_trade,
+                size=sell_size,
                 price=sell_price,
+                time_in_force=pyRofex.TimeInForce.FillOrKill,
                 order_type=pyRofex.OrderType.LIMIT)
+
+            trade_info = [
+                f'--- Trade Info For Tenure {maturity_tag} ---',
+                'Rate long side:',
+                f'Buy:      {ticker_to_buy:<12} -> {buy_size:>8} @ {buy_price}',
+                f'Sell:     {underlier_to_sell:<12} -> {underlier_sell_size:>8} @ {underlier_sell_price}',
+                f'Imp Rate: {min_offered_rate}',
+                f'Rate short side:',
+                f'Sell:     {ticker_to_sell:<12} -> {sell_size:>8} @ {sell_price}',
+                f'Buy:      {underlier_to_buy:<12} -> {underlier_buy_size:>8} @ {underlier_buy_price}',
+                f'Imp Rate: {max_taker_rate}',
+                f'--------------------------------------------',
+                f'Trade rate profit:     {trade_rate_profit}',
+                f'Average position size: {av_position_to_take}',
+                f'--------------------------------------------']
+
+            print('\n'.join(trade_info), flush=True)
+
